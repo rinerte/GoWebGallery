d3b1d80e2e251b4067ce9bdb65121cbecb9e30f6
rinerte <rinerte@gmail.com>
2025-03-26 11:00:30 +0900

    add 404

--- main.go
+++ main.go
@@ -11,10 +11,15 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Any page</h1><p>with any text</p>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "<a href=\"t.me/rinerte\">My telegram </a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We did not found page you are looking for</h1><p>Go love yourself</p>")
 	}
 }
 
 func main() {
+	fmt.Println("Server started on http://localhost:3000")
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
+
 }
