347f2abf3c5d800001f01f376ee68d66bd030d99
rinerte <rinerte@gmail.com>
2025-03-27 10:19:45 +0900

    chapter 3 with excercises ended

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rinerte/GoWebGallery
 
 go 1.24.0
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -3,23 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Any page</h1><p>with any text</p>")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "<a href=\"t.me/rinerte\">My telegram </a>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>We did not found page you are looking for</h1><p>Go love yourself</p>")
-	}
+	fmt.Fprint(w, "<h1>Home page</h1><p>with any text</p>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<a href=\"t.me/rinerte\">My telegram </a>")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ:</h1><p>SHITSUMON: Kore wa nani?</p><p>KOTAE: Kore wa webupeji desu</p>")
+}
+
+func not_found(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Page not found</h1>")
 }
 
 func main() {
 	fmt.Println("Server started on http://localhost:3000")
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	h := http.HandlerFunc(not_found)
+	r.NotFoundHandler = h
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	http.ListenAndServe(":3000", r)
 
 }
